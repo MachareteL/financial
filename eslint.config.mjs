eceed0e3faa01dbd673fc4052e104a0f28f47e7f
mlc4ca <mlc4ca@bosch.com>
2025-11-30 11:46:16 -0300

    feat: Upgrade ESLint to v9 and configure flat config with Next.js, TypeScript, and Prettier.

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+];
+
+export default eslintConfig;
